9ea69bc79e08fb47124e06dac27245d85704279a
unknown <ouerghi.chaima@gmail.com>
2024-03-07 15:24:00 +0100

    '......'

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -9,26 +9,48 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    public function index(Request $request,
+     EntityManagerInterface $entityManager,
+     ValidatorInterface $validator): Response
     {
             $contact = new Contact();
     
             $form = $this->createForm(ContactType::class, $contact);
             $form->handleRequest($request); // permet d'intercepter la requete lancé par la soumission du formulaire
     
-            if($form->isSubmitted() && $form->isValid()){
-                $entityManager->persist($contact); //inserer en base
-                $entityManager->flush(); //fermer la transaction apr la bdd
+            if ($form->isSubmitted()) {
 
-
+                if ($form->isValid()) {
+    
+                    $contact->setDate(new \DateTime);
+                    $entityManager->persist($contact); // insérer en base
+                    $entityManager->flush(); // fermer la transaction executée par la bdd
+    
+                    $this->addFlash(
+                        'success',
+                        'Votre message a bien été envoyé'
+                    );
+    
+                    // rediriger vers une autre page
+                    // return $this->redirectToRoute(/* ... */);
+                }
+                // } else {
+                //     $errors = $validator->validate($contact);
+                // }
+    
             }
-        return $this->render('contact/index.html.twig', [
-
-            'contactForm' => $form,
-        ]);
+    
+            return $this->render('contact/index.html.twig', [
+                'contactForm' => $form,
+                'errors' => !isset($errors) ? NULL : $errors
+            ]);
+        }
     }
-}
+    
